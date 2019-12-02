3f6ccdde9f9a70cab2dda59d166df1ee5d6b1121
Tom Shiflet <tom.shiflet@mylcs.com>
2019-12-01 20:31:24 -0600

    Init

--- BodyTester/GameScene.swift
+++ BodyTester/GameScene.swift
@@ -11,55 +11,39 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+
+    let body=SKSpriteNode(imageNamed: "body00")
+    let head=SKSpriteNode(imageNamed: "head00")
+    let legs=SKSpriteNode(imageNamed: "legs00")
     
+    var bScale:CGFloat=8.0
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        addChild(body)
+        body.texture?.filteringMode=SKTextureFilteringMode.nearest
+        head.texture?.filteringMode=SKTextureFilteringMode.nearest
+        legs.texture?.filteringMode=SKTextureFilteringMode.nearest
+        body.addChild(head)
+        body.addChild(legs)
+        body.setScale(bScale)
+        head.position.y = body.size.height/(body.yScale)
+        legs.position.y = -body.size.height/(body.yScale)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        body.run(SKAction.repeatForever(SKAction.sequence([SKAction.scale(to: bScale*1.2, duration: 0.33),SKAction.scale(to: bScale, duration: 0.33)])))
+
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
     override func mouseDown(with event: NSEvent) {
@@ -76,10 +60,20 @@ class GameScene: SKScene {
     
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
+
+        case 49:
+            let legInt=Int(random(min: 0, max: 1.99999999))
+            let bodyInt=Int(random(min: 0, max: 1.99999999))
+            let headInt=Int(random(min: 0, max: 1.99999999))
+            
+            body.texture=SKTexture(imageNamed: "body0\(bodyInt)")
+            legs.texture=SKTexture(imageNamed: "legs0\(legInt)")
+            head.texture=SKTexture(imageNamed: "head0\(headInt)")
+            
+            body.texture?.filteringMode=SKTextureFilteringMode.nearest
+            head.texture?.filteringMode=SKTextureFilteringMode.nearest
+            legs.texture?.filteringMode=SKTextureFilteringMode.nearest
+            
         default:
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
