d5d65f8b3fa89b905cbe2f66cf60dc52fcaf9b3c
Tom Shiflet <tom.shiflet@mylcs.com>
2019-12-02 06:47:34 -0600

    3 variations

--- BodyTester/GameScene.swift
+++ BodyTester/GameScene.swift
@@ -15,9 +15,22 @@ class GameScene: SKScene {
     let body=SKSpriteNode(imageNamed: "body00")
     let head=SKSpriteNode(imageNamed: "head00")
     let legs=SKSpriteNode(imageNamed: "legs00")
+    let nameLabel=SKLabelNode(fontNamed: "Chalkduster")
+    
+    let NUMVAR:Int=3
+    
+    let headName=["Veron","Quack","Scorp"]
+    let bodyName=["a", "ano", "io"]
+    let legName=["ni", "lo", "so"]
+    
+    var creatureName=""
     
     var bScale:CGFloat=8.0
     override func didMove(to view: SKView) {
+        
+        nameLabel.position.y=size.height*0.45
+        addChild(nameLabel)
+        
         
         addChild(body)
         body.texture?.filteringMode=SKTextureFilteringMode.nearest
@@ -31,6 +44,9 @@ class GameScene: SKScene {
         
         body.run(SKAction.repeatForever(SKAction.sequence([SKAction.scale(to: bScale*1.2, duration: 0.33),SKAction.scale(to: bScale, duration: 0.33)])))
 
+        createCreature()
+        
+        
     }
     
     
@@ -58,22 +74,32 @@ class GameScene: SKScene {
         self.touchUp(atPoint: event.location(in: self))
     }
     
+    func createCreature()
+    {
+        let legInt=Int(random(min: 0, max: CGFloat(NUMVAR)-0.00000001))
+        let bodyInt=Int(random(min: 0, max: CGFloat(NUMVAR)-0.00000001))
+        let headInt=Int(random(min: 0, max: CGFloat(NUMVAR)-0.00000001))
+        
+        body.texture=SKTexture(imageNamed: "body0\(bodyInt)")
+        legs.texture=SKTexture(imageNamed: "legs0\(legInt)")
+        head.texture=SKTexture(imageNamed: "head0\(headInt)")
+        
+        body.texture?.filteringMode=SKTextureFilteringMode.nearest
+        head.texture?.filteringMode=SKTextureFilteringMode.nearest
+        legs.texture?.filteringMode=SKTextureFilteringMode.nearest
+        
+        creatureName=headName[headInt]+bodyName[bodyInt]+legName[legInt]
+        print(creatureName)
+        nameLabel.text=creatureName
+        
+        
+    }
+    
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
 
         case 49:
-            let legInt=Int(random(min: 0, max: 1.99999999))
-            let bodyInt=Int(random(min: 0, max: 1.99999999))
-            let headInt=Int(random(min: 0, max: 1.99999999))
-            
-            body.texture=SKTexture(imageNamed: "body0\(bodyInt)")
-            legs.texture=SKTexture(imageNamed: "legs0\(legInt)")
-            head.texture=SKTexture(imageNamed: "head0\(headInt)")
-            
-            body.texture?.filteringMode=SKTextureFilteringMode.nearest
-            head.texture?.filteringMode=SKTextureFilteringMode.nearest
-            legs.texture?.filteringMode=SKTextureFilteringMode.nearest
-            
+            createCreature()
         default:
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
